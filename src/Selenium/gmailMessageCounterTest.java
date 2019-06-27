30f8e0dec94dd8a25f6573e4eea9765733341e7d
george <georgb5225@gmail.com>
2019-06-27 05:58:51 +0300

    updated

--- src/Selenium/gmailMessageCounterTest.java
+++ src/Selenium/gmailMessageCounterTest.java
@@ -2,22 +2,42 @@ package Selenium;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
 
 public class gmailMessageCounterTest {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
+        //Tesfaye Berhanu ...ATR/4668/09.....Sectio:3
+
+
         System.setProperty("webdriver.chrome.driver","/home/george/Downloads/Compressed/chromedriver_linux64_3/chromedriver");
         WebDriver driver;
         driver = new ChromeDriver();
-        driver.get("https://www.google.com/gmail/");
-        System.out.println("Successfully opened the website ");
-        driver.findElement(By.className("whsOnd zHQkBf")).sendKeys("georgb5225@gmail.com");
-        driver.findElement(By.className("RveJvd snByac")).click();
-        driver.findElement(By.className("whsOnd zHQkBf")).sendKeys("@United52");
-        driver.findElement(By.className("RveJvd snByac")).click();
-      //  driver.findElements(By.className(""))
 
+        driver.get("http://mail.google.com");
+        driver.findElement(By.xpath("//input[@name='identifier']")).sendKeys("email");
+        driver.findElement(By.xpath("//*[@id=\"identifierNext\"]")).click();
+        Thread.sleep(10000);
+        WebElement pass = driver.findElement(By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input"));
+        pass.click();
+        pass.sendKeys("password");
+        driver.findElement(By.xpath("//*[@id=\"passwordNext\"]")).click();
+
+        Thread.sleep(1000);
+        List<WebElement> unread = driver.findElements(By.xpath("//*[@class='zA zE']"));
+        System.out.println(unread.size() + " Unread Messages");
+        FileWriter fw=new FileWriter("test.txt");
+        for (WebElement message: unread  ) {
+            System.out.println(message.getText());
+            fw.write(message.getText()+"\n");
+
+        }
+        fw.close();
     }
 
 }
